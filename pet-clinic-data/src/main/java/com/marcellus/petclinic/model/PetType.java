60811d61819a7627e688af7efe9456e6ad2643ce
Maciej Moniuszko <moniuszko@gmail.com>
2020-02-08 16:24:03 +0100

    Owners, Pet, PetType converted to entities. Closes #36.

--- pet-clinic-data/src/main/java/com/marcellus/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/marcellus/petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package com.marcellus.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
